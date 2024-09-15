7d10bb53e1c540767ea97e1feca46a7489977686
User <2381058@unai.edu>
2024-09-15 09:47:32 +0700

    Handson 3

---
+++ handson1/ForEach.java
@@ -0,0 +1,12 @@
+package handson1;
+
+import java.sql.SQLOutput;
+
+public class ForEach {
+    public static void main(String[] args) {
+        String[] kumpulanBuah = {"Apel", "Pisang", "Mangga", "Jeruk", "Durian", "Anggur"};
+        for (String buah : kumpulanBuah){
+            System.out.println(buah);
+        }
+    }
+}
